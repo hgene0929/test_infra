913ddfffea5148d5cada890116e4270d437bf17a
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-19 01:00:44 +0900

    git commit test

---
+++ src/main/java/com/example/infra/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.infra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	@GetMapping
+	public String healthCheck(){
+		return "health ok";
+	}
+}
+
